6b77de91f01e7921c31fae5b716276400c0cdb4c
Andrey <119668295+AndreyZabolotnikov@users.noreply.github.com>
2024-02-25 20:25:50 +0500

    add

---
+++ src/main/java/ru/duxa/stairweb/model/Person.java
@@ -0,0 +1,72 @@
+package ru.duxa.stairweb.model;
+
+public class Person {
+    private int id;
+    private String name;
+    private String middleName;
+    private String lastName;
+    private String organization;
+    private int telephone;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
