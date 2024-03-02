b024fc7de22567edccee286750e63e3c04e8969e
Andrey <119668295+AndreyZabolotnikov@users.noreply.github.com>
2024-03-02 12:26:30 +0500

    add

--- src/main/java/ru/duxa/stairweb/controller/StartController.java
+++ src/main/java/ru/duxa/stairweb/controller/StartController.java
@@ -1,16 +1,11 @@
 package ru.duxa.stairweb.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.duxa.stairweb.repository.PersonRepository;
+
 
 @Controller
 public class StartController {
-
-    @Autowired
-    private  PersonRepository personRepository;
-
     @GetMapping
     public String startWeb(){
         return "index";
--- src/main/java/ru/duxa/stairweb/model/Person.java
+++ src/main/java/ru/duxa/stairweb/model/Person.java
@@ -3,23 +3,21 @@ package ru.duxa.stairweb.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.management.relation.Role;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "person")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class Person {
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
 
     @Column(name = "name")
     @NotBlank(message = "Имя не может быть пустым")
@@ -36,7 +34,7 @@ public class Person {
 
     @Column(name = "telephone")
     @NotBlank(message = "Номер телефона не может быть пустым")
-    private int telephone;
+    private Long telephone;
 
     @Column(name = "email", unique = true)
     @NotBlank(message = "Email не может быть пустым")
@@ -55,64 +53,4 @@ public class Person {
         dateCreate = LocalDateTime.now();
     }
 
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getOrganization() {
-        return organization;
-    }
-
-    public void setOrganization(String organization) {
-        this.organization = organization;
-    }
-
-    public int getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(int telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
