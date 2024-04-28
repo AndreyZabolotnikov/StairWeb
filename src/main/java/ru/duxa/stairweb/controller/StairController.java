fa5be642d68f4212455400f7fa4675317ad7119f
Andrey <119668295+AndreyZabolotnikov@users.noreply.github.com>
2024-04-28 09:25:35 +0500

    add

--- src/main/java/ru/duxa/stairweb/controller/StairController.java
+++ src/main/java/ru/duxa/stairweb/controller/StairController.java
@@ -2,7 +2,6 @@ package ru.duxa.stairweb.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/ru/duxa/stairweb/dto/StairDto.java
+++ src/main/java/ru/duxa/stairweb/dto/StairDto.java
@@ -1,6 +1,7 @@
 package ru.duxa.stairweb.dto;
 
 import jakarta.persistence.ElementCollection;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,13 +16,16 @@ import java.util.Map;
 public class StairDto {
     private int numberStair;
     @NotNull(message = "не заполнено")
-    @Min(value = 1, message = "не заполнено")
+    @Min(value = 1, message = "значение не может быть меньше 1")
+    @Max(value = 10000, message = "значение не может быть больше 10000")
     private Integer upperFloor;
     @NotNull(message = "не заполнено")
-    @Min(value = 1, message = "не заполнено")
+    @Min(value = 1, message = "значение не может быть меньше 1")
+    @Max(value = 10000, message = "значение не может быть больше 10000")
     private Integer downFloor;
     @NotNull(message = "не заполнено")
-    @Min(value = 1, message = "не заполнено")
+    @Min(value = 1, message = "значение не может быть меньше 1")
+    @Max(value = 10000, message = "значение не может быть больше 10000")
     private Integer widthStair;
     @ElementCollection
     private Map<Integer, Integer> stepHeights;
